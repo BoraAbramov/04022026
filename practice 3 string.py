086d8f7c7f1c5595f21348aa3b2ae1069c399fb4
BoraAbramov <borya812@gmail.com>
2026-02-04 20:51:22 +0200

    update

---
+++ practice 3 string.py
@@ -0,0 +1,20 @@
+
+_first_name = str("jane")
+
+_last_name = str("doe")
+
+_id = str("123456")
+
+_phone = str("555-789-325")
+
+_birth_year = int(1996)
+
+_vegetarian = False
+
+_exact_age = float(25.6)
+
+print(f"your name is {_first_name} {_last_name} and your phone is {_phone} you was born on {_birth_year} and you {_vegetarian}")
+
+
+print(str.upper(_first_name)) #פעולה מבחוץ
+print(_first_name.upper()) #פעולה מבפנים
